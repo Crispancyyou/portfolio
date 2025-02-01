f3f7602c6aea40ea8291c3428de9f10bfde040f3
JoshEdison <joshuabedison@gmail.com>
2025-02-01 08:09:44 -0500

    yes

--- routes/index.js
+++ routes/index.js
@@ -21,38 +21,7 @@ router.get("/contact", function (req, res, next) {
   res.render("contact", { title: "Contact" });
 });
 
-router.get("/forex", function (req, res, next) {
-  res.render("forex", { title: "Forex" });
-});
 
-/**
- * NEW ROUTE: Return the entire forex_history.txt as JSON
- * Assuming forex_history.txt is literally in the same folder as index.js
- */
-router.get("/forex/history", function (req, res) {
-  try {
-    // Build the path to forex_history.txt in the same directory as index.js
-    const HISTORY_FILE = path.join(__dirname, "forex_history.txt");
 
-    // If file doesn't exist, return an empty array
-    if (!fs.existsSync(HISTORY_FILE)) {
-      return res.json([]);
-    }
-
-    const content = fs.readFileSync(HISTORY_FILE, "utf8").trim();
-    if (!content) {
-      return res.json([]);
-    }
-
-    // Each line is one JSON object
-    const lines = content.split("\n");
-    const historyData = lines.map((line) => JSON.parse(line));
-
-    res.json(historyData);
-  } catch (err) {
-    console.error("Error reading forex history file:", err);
-    res.status(500).json({ error: "Unable to read history file" });
-  }
-});
 
 module.exports = router;
